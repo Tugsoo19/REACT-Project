faf2b62427661bc71d9f423b7d4d2f6233bccf60
tugsoo19 <tugswork@gmail.com>
2023-01-14 23:37:13 +0800

    latest update last

--- e-commerce/src/components/LatestProducts.jsx
+++ e-commerce/src/components/LatestProducts.jsx
@@ -5,9 +5,12 @@ const LatestProdFunc = latestProdData.map((el) => {
     <div className="d-flex border rounded-5 gap-5 p-4">
       <img src={el.image}></img>
       <div className="latest-rightside">
-        <a>{el.date}</a>
-        <h4>{el.title}</h4>
-        {el.text}
+        <a className="more-link border rounded-5 py-2 px-3">{el.date}</a>
+        <h1 className="section-title my-3 ">{el.title}</h1>
+        <p>{el.text}</p>
+        <br/>
+        <br/>
+        <br/>
         <p>{el.by}</p>
       </div>
     </div>
